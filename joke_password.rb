d9c5f91459b8f5dadcbb4f295d07c76a9631a960
Alex Maisiuk <alexthunderstorm07@gmail.com>
2017-10-04 22:50:56 +0300

    Tiny-Piny

--- joke_password.rb
+++ joke_password.rb
@@ -12,17 +12,22 @@ nouns = %w[
 ]
 
 puts 'Welcome to Joke Password Generator!'
-
-while true
-  3.times do
-    adjective = adjectives.sample
-    noun = nouns.sample
-    number = rand(0...500) + 1
-    special_char = (' '..'?').to_a.sample
-    password = adjective + noun + number.to_s + special_char.to_s
-    puts "Maybe this password is: #{password}"
+print 'Do you get your own securable password? If you need it type y: '
+okay = gets.chomp
+if okay == 'y'
+  while true
+    3.times do
+      adjective = adjectives.sample
+      noun = nouns.sample
+      number = rand(0...500) + 1
+      special_char = (' '..'?').to_a.sample
+      password = adjective + noun + number.to_s + special_char.to_s
+      puts "Maybe this password is: #{password}"
+    end
+    print 'Would you like another password? Type y or n: '
+    response = gets.chomp
+    break if response == 'n'
   end
-  print 'Would you like another password? Type y or n: '
-  response = gets.chomp
-  break if response == 'n'
+else
+  puts "And you didn't need your own securable password. Bye!"
 end
