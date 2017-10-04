59d75258d3d8178fb622a9ca43ea3be18b053798
Alex Maisiuk <alexthunderstorm07@gmail.com>
2017-10-04 21:44:20 +0300

    Simple Refactoring

--- joke_password.rb
+++ joke_password.rb
@@ -1,9 +1,23 @@
-adjectives = [:sleepy, :slow, :smelly, :wet, :fat, :red,
-              :orange, :yellow, :green, :blue, :purple, :fluffy,
-              :white, :proud, :brave]
+adjectives = ['sleepy', 'slow', 'smelly', 'wet', 'fat', 'red',
+              'orange', 'yellow', 'green', 'blue', 'purple', 'fluffy',
+              'white', 'proud', 'brave']
 
-nouns = [:apple, :dinosaur, :ball,
-         :toaster, :goat, :dragon,
-         :hammer, :duck, :panda]
+nouns = ['apple', 'dinosaur', 'ball',
+         'toaster', 'goat', 'dragon',
+         'hammer', 'duck', 'panda']
 
-puts 'Welcome to Password Picker!'
+puts 'Welcome to Joke Password Generator!'
+
+while true
+adjective = adjectives.sample
+noun = nouns.sample
+number = rand(0...500)
+# special_char = random.choice(string.punctuation)
+
+password = adjective + noun + number.to_s # + special_char
+puts "Your new password is: #{password}"
+
+print 'Would you like another password? Type y or n: '
+response = gets.chomp
+break if response == 'n'
+end
