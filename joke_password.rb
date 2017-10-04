1274e62e6b232ab9c10121e33caf62aaee5037ea
Alex Maisiuk <alexthunderstorm07@gmail.com>
2017-10-04 22:37:46 +0300

    Refactoring... ))

--- joke_password.rb
+++ joke_password.rb
@@ -1,23 +1,28 @@
-adjectives = ['sleepy', 'slow', 'smelly', 'wet', 'fat', 'red',
-              'orange', 'yellow', 'green', 'blue', 'purple', 'fluffy',
-              'white', 'proud', 'brave']
+adjectives = %w[
+  sleepy slow smelly wet fat red
+  orange yellow green blue purple fluffy
+  white proud brave
+]
 
-nouns = ['apple', 'dinosaur', 'ball',
-         'toaster', 'goat', 'dragon',
-         'hammer', 'duck', 'panda']
+nouns = %w[
+  apple dinosaur ball
+  toaster goat dragon
+  hammer duck panda
+  telephone banana teacher
+]
 
 puts 'Welcome to Joke Password Generator!'
 
 while true
-adjective = adjectives.sample
-noun = nouns.sample
-number = rand(0...500)
-# special_char = random.choice(string.punctuation)
-
-password = adjective + noun + number.to_s # + special_char
-puts "Your new password is: #{password}"
-
-print 'Would you like another password? Type y or n: '
-response = gets.chomp
-break if response == 'n'
+  3.times do
+    adjective = adjectives.sample
+    noun = nouns.sample
+    number = rand(0...500) + 1
+    special_char = (' '..'?').to_a.sample
+    password = adjective + noun + number.to_s + special_char.to_s
+    puts "Maybe this password is: #{password}"
+  end
+  print 'Would you like another password? Type y or n: '
+  response = gets.chomp
+  break if response == 'n'
 end
