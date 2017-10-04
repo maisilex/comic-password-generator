d5038453c2fcab5f0e835847c63ba5b8be8256bc
Alex Maisiuk <alexthunderstorm07@gmail.com>
2017-10-04 20:56:40 +0300

    Welcome to Joke Password Generator!

---
+++ joke_password.rb
@@ -0,0 +1,9 @@
+adjectives = [:sleepy, :slow, :smelly, :wet, :fat, :red,
+              :orange, :yellow, :green, :blue, :purple, :fluffy,
+              :white, :proud, :brave]
+
+nouns = [:apple, :dinosaur, :ball,
+         :toaster, :goat, :dragon,
+         :hammer, :duck, :panda]
+
+puts 'Welcome to Password Picker!'
